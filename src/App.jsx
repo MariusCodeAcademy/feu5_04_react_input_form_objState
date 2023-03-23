4649d023cd234389adef4b716b091e40c1c1e588
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-23 12:29:00 +0200

    TodoListStart

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './styles/App.css';
 import InputBlock from './components/InputBlock';
 import PassBlock from './components/PassBlock';
 import LoginForm from './components/Login/LoginForm';
+import TodoList from './components/Todo/TodoList';
 
 function App() {
   return (
     <div className='container'>
       <h1>Inputs</h1>
+      <TodoList />
       {/* <LoginForm /> */}
       {/* <InputBlock /> */}
       {/* <PassBlock /> */}
--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,3 +1,34 @@
 // sukurti componenta
 // div, input, button, ul > li
 // arr = [{ id: 1, title: 'buy milk'}]
+
+import { useState } from 'react';
+
+const initTodos = [
+  { id: '1', title: 'buy milk', isDone: false },
+  { id: '12', title: 'Go to Mall', isDone: false },
+  { id: '234', title: 'Lear React', isDone: true },
+  { id: '435345', title: 'Watch tv', isDone: false },
+];
+
+function TodoList() {
+  const [mainTodoArr, setMainTodoArr] = useState(initTodos);
+  return (
+    <div>
+      <h3>TodoList</h3>
+      <fieldset>
+        <legend>Add todo</legend>
+        <input type='text' placeholder='What todo' />
+        <button>Add</button>
+      </fieldset>
+
+      <ul className='todos'>
+        {mainTodoArr.map((tObj) => (
+          <li>Buy milk</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
