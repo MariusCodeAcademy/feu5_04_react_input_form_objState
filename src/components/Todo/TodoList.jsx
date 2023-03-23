df24f1939550cad91f68d8caf430341e9c771001
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-23 12:50:07 +0200

    addTodo

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -5,26 +5,54 @@
 import { useState } from 'react';
 
 const initTodos = [
-  { id: '1', title: 'buy milk', isDone: false },
+  { id: '1', title: 'Buy milk', isDone: false },
   { id: '12', title: 'Go to Mall', isDone: false },
-  { id: '234', title: 'Lear React', isDone: true },
+  { id: '234', title: 'Learn React', isDone: true },
   { id: '435345', title: 'Watch tv', isDone: false },
 ];
 
 function TodoList() {
+  //
   const [mainTodoArr, setMainTodoArr] = useState(initTodos);
+  const [newTodoValue, setNewTodoValue] = useState('');
+
+  function handleNewTodoInput(e) {
+    setNewTodoValue(e.target.value);
+  }
+
+  function addTodo() {
+    // sugeneruoti naujo todo objekta
+    const newTodoObj = {
+      id: Math.random().toString().slice(2),
+      title: newTodoValue,
+      isDone: false,
+    };
+    console.log('newTodoObj ===', newTodoObj);
+    // prideti prie esamo state
+    // setMainTodoArr(paduoti masyva kaip noriu kad atrodyti po pakeitimo, nemutuojam esamo mainTodoArr)
+    const stateCopyWithNewTodo = [...mainTodoArr, newTodoObj];
+
+    // atnaujinti state
+    setMainTodoArr((prevMainTodoArr) => [...prevMainTodoArr, newTodoObj]);
+  }
+
   return (
     <div>
       <h3>TodoList</h3>
       <fieldset>
         <legend>Add todo</legend>
-        <input type='text' placeholder='What todo' />
-        <button>Add</button>
+        <input
+          value={newTodoValue}
+          onChange={handleNewTodoInput}
+          type='text'
+          placeholder='What todo'
+        />
+        <button onClick={addTodo}>Add</button>
       </fieldset>
 
       <ul className='todos'>
         {mainTodoArr.map((tObj) => (
-          <li>Buy milk</li>
+          <li key={tObj.id}>{tObj.title}</li>
         ))}
       </ul>
     </div>
