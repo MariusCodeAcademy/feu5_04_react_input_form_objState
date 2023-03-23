a445cebfa0fe7c08fadd162fce7ecc619dc8cd10
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-23 11:33:25 +0200

    bind inputs

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,4 +1,17 @@
+import { useState } from 'react';
+
 function LoginForm() {
+  const [emailVal, setEmailVal] = useState('');
+  const [passValue, setPassValue] = useState('');
+  const [showOutputs, setShowOutputs] = useState(false);
+
+  function handleEmailInput(e) {
+    setEmailVal(e.target.value);
+  }
+  function handlePassInput(e) {
+    setPassValue(e.target.value);
+  }
+
   //
   function handleSubmit(event) {
     event.preventDefault();
@@ -9,17 +22,24 @@ function LoginForm() {
     <div>
       <h3>Login here</h3>
       <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='email' />
-        <input type='password' placeholder='password' />
+        <input value={emailVal} onChange={handleEmailInput} type='text' placeholder='email' />
+        <input
+          value={passValue}
+          onChange={handlePassInput}
+          type='password'
+          placeholder='password'
+        />
         <button type='submit'>Login</button>
       </form>
       <hr />
-      <button>Rodyti ivestis</button>
-      <fieldset>
-        <legend>Ivesta</legend>
-        <p>Email: </p>
-        <p>Password: </p>
-      </fieldset>
+      <button onClick={() => setShowOutputs(true)}>Rodyti ivestis</button>
+      {showOutputs && (
+        <fieldset>
+          <legend>Ivesta</legend>
+          <p>Email: {emailVal}</p>
+          <p>Password: {passValue}</p>
+        </fieldset>
+      )}
     </div>
   );
 }
